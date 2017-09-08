e46fab0c478a4a2ada298b41ba45664a17e1f2a7
lababama <lababama@gmail.com>
2017-09-08 14:13:45 +0300

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,14 @@
+var Sveikinimas = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>My Oh My!</h1>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Sveikinimas/>,
+document.getElementById('app')
+);
