97121626321ecf28507cd5dc9616d58f352f79ed
lababama <lababama@gmail.com>
2017-09-08 18:02:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var Sveikinimas = React.createClass({
+var Main = React.createClass({
     render: function () {
         return (
             <div>
+                <Nav/>
                 <h1>My Oh My!</h1>
             </div>
         );
     }
 });
 
+var Nav = React.createClass({
+    render: function () {
+        return (
+            <h3>Nav component</h3>
+            )
+    }
+});
+
 ReactDOM.render(
-    <Sveikinimas/>,
+    <Main/>,
 document.getElementById('app')
 );
